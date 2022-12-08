ca168ac7738cd0dae950b293189120aa9a4b0324
yucopowo <yucopowo@gmail.com>
2022-12-08 17:52:18 +0800

    全面去除web component 用页面来实现样式隔离

---
+++ cdn/@mapbox/rehype-prism.js
@@ -0,0 +1,3 @@
+/* esm.sh - @mapbox/rehype-prism@0.8.0 */
+export * from '/cdn/v99/@mapbox/rehype-prism@0.8.0/es2022/rehype-prism.development.js';
+export { default } from '/cdn/v99/@mapbox/rehype-prism@0.8.0/es2022/rehype-prism.development.js';
